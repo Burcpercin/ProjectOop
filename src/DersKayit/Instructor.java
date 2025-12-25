44f2157aee6fb71f8e286fac3a12c2b42ce3598e
burcp <burcp@Victus_16>
2025-12-25 15:16:06 +0300

    Instructor classına temel özellikler tanımlandı ve getterları yazılıp eklendi.

--- src/DersKayit/Instructor.java
+++ src/DersKayit/Instructor.java
@@ -1,5 +1,18 @@
 package DersKayit;
 
-public class Instructor {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Instructor {
+    private String name;
+    private String department;
+    private List<Courses> givenCourses;
+
+    public Instructor(String name, String department) {
+        this.name = name;
+        this.department = department;
+        this.givenCourses = new ArrayList<>();
+    }
+
+  
 }
