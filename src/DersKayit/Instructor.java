d3a850787be972b1d743e626a0d053c8b4fd5790
burcp <burcp@Victus_16>
2025-12-25 15:34:14 +0300

    Instructor class ına ders ekleme metotları eklendi.

--- src/DersKayit/Instructor.java
+++ src/DersKayit/Instructor.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Instructor {
     private String name;
     private String department;
-    private List<Courses> givenCourses;
+    private List<Course> givenCourses;
 
     public Instructor(String name, String department) {
         this.name = name;
@@ -14,5 +14,17 @@ public class Instructor {
         this.givenCourses = new ArrayList<>();
     }
 
-  
+    public void addCourseToTeach(Course course) {
+        // Hoca için sadece zaman kontrolü yapılır
+        for (Course c : givenCourses) {
+            if (c.hasConflict(course)) {
+                System.out.println("HATA: Bu tarihte başka dersiniz var!");
+                return;
+            }
+        }
+        givenCourses.add(course);
+        System.out.println("Ders programınıza eklendi: " + course.getCourseName());
+    }
+
+    public String getName() { return name; }
 }
