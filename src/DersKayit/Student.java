2bbcddf7da2acb8de85f9ed1dc12fb84c90520dc
burcp <burcp@Victus_16>
2025-12-26 22:58:21 +0300

    Student classına registrable değişkenleri tanımlandı, getter setterları oluşturuldu ve enrolledCourses adlı oluşturuldu.

--- src/DersKayit/Student.java
+++ src/DersKayit/Student.java
@@ -1,10 +1,25 @@
 package DersKayit;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Student implements Registrable {
+    private String studentNumber; 
+    private String name;
+    private int gradeLevel;
+    
+    private List<Course> enrolledCourses;
+
+    public Student(String studentNumber, String name, int gradeLevel) {
+        this.studentNumber = studentNumber;
+        this.name = name;
+        this.gradeLevel = gradeLevel;
+        this.enrolledCourses = new ArrayList<>();
+    }
 
 	@Override
-	public void registerForCourse(Courses course) {
+	public void registerForCourse(Course course) {
 		
 	}
-
 }
