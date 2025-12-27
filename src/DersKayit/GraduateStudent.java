167b3a18a2a1e000d0a9daa47538c738d7e85e76
burcp <burcp@Victus_16>
2025-12-27 21:49:32 +0300

    Kalıtım ile özellikler student classından alındı ve farklı olark tez eklendi.

--- src/DersKayit/GraduateStudent.java
+++ src/DersKayit/GraduateStudent.java
@@ -1,6 +1,10 @@
 package DersKayit;
 
 public class GraduateStudent extends Student {
+    private String thesisTopic;
 
-	}
-
+    public GraduateStudent(String username, String name, int gradeLevel, String thesisTopic) {
+        super(username, name, gradeLevel);
+        this.thesisTopic = thesisTopic;
+    }
+}
