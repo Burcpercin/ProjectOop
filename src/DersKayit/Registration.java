a42e9a29347cf84d2675fd177fcd30e8acfe10a9
burcp <burcp@Victus_16>
2025-12-28 15:05:36 +0300

    Gerekli değişkenler tanımlandı.

--- src/DersKayit/Registration.java
+++ src/DersKayit/Registration.java
@@ -1,5 +1,19 @@
 package DersKayit;
 
+import java.time.LocalDateTime;
+
 public class Registration {
 
+	    private Student student;
+	    private Course course;
+	    private LocalDateTime registrationDate;
+	    private String status; // "Registered", "Dropped" vb.
+
+	    public Registration(Student student, Course course) {
+	        this.student = student;
+	        this.course = course;
+	        this.registrationDate = LocalDateTime.now();
+	        this.status = "Attempting";
+	    }
+	
 }
