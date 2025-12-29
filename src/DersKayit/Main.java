e1012c8590511438fa008ed3cdfd63c4ad96fff3
burcp <burcp@Victus_16>
2025-12-30 00:08:32 +0300

    Kullanıcı girişi tekrar elden geçirildi, arayüz değiştirmek amacıyla kaldırıldı.

--- src/DersKayit/Main.java
+++ src/DersKayit/Main.java
@@ -1,57 +1,31 @@
 package DersKayit;
 
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-       Authentication authService = new Authentication();
+        Authentication authService = new Authentication();
         
-        // --- 1. GİRİŞ EKRANI ---
-        System.out.println("=== ÖĞRENCİ BİLGİ SİSTEMİNE HOŞGELDİNİZ ===");
-        
-        while (true) { //Ana döngü
-            System.out.print("Kullanıcı Adı: ");
+        // Katalog başlarken 'courses.csv' dosyasını okur ve hazırlar
+        CourseCatalog courseCatalog = new CourseCatalog();
+
+        System.out.println("=== ÖĞRENCİ DERS KAYIT SİSTEMİ ===");
+
+        while (true) {
+            System.out.print("\nKullanıcı Adı: ");
             String user = scanner.nextLine();
-            
             System.out.print("Şifre: ");
             String pass = scanner.nextLine();
-            
-            // CSV kontrolü yapılıyor
+
             String[] userData = authService.login(user, pass);
-            
+
             if (userData != null) {
-                System.out.println("Giriş Başarılı! Hoşgeldin " + userData[3]);
-                String role = userData[2]; // instructor veya student
-                
-                // Role göre menü aç
-                if (role.equals("instructor")) {
-                    showInstructorMenu(scanner);
-                } else if (role.equals("student")) {
-                    showStudentMenu(scanner);
-                }
-                break;
-            } else {
-                System.out.println("Hatalı kullanıcı adı veya şifre! Tekrar dene.\n");
+                String role = userData[2];
+                String name = userData[3];
+                System.out.println("Giriş Başarılı! Hoşgeldin " + name);
             }
         }
-    }
-
-    // --- ÖĞRETMEN MENÜSÜ ---
-    public static void showInstructorMenu(Scanner scanner) {
-        System.out.println("\n--- ÖĞRETMEN PANELİ ---");
-        System.out.println("1. Ders Ekle");
-        System.out.println("2. Öğrenci Listele");
-        System.out.println("3. Çıkış");
-        // Buraya switch-case ile seçim işlemleri gelecek
-        int secim = scanner.nextInt();
-    }
-
-    // --- ÖĞRENCİ MENÜSÜ ---
-    public static void showStudentMenu(Scanner scanner) {
-        System.out.println("\n--- ÖĞRENCİ PANELİ ---");
-        System.out.println("1. Derslere Göz At");
-        System.out.println("2. Derse Kayıt Ol");
-        System.out.println("3. Ders Programımı Gör");
     }
 }
