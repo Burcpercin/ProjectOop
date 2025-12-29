ff28eb9040d75dc180c8b77df60aa1089a74ab4e
burcp <burcp@Victus_16>
2025-12-30 02:16:54 +0300

    Kontrol mekanizması, kullanıcının türünü if-else yerine switch case ile kontrol edilecek şekilde değiştirildi.

--- src/DersKayit/Main.java
+++ src/DersKayit/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Authentication authService = new Authentication();
         
-        // Katalog başlarken 'courses.csv' dosyasını okur ve hazırlar
+        // Katalog, program açılınca 'courses.csv' dosyasını okur
         CourseCatalog courseCatalog = new CourseCatalog();
 
         System.out.println("=== ÖĞRENCİ DERS KAYIT SİSTEMİ ===");
@@ -24,33 +24,40 @@ public class Main {
             if (userData != null) {
                 String role = userData[2];
                 String name = userData[3];
-                System.out.println("Giriş Başarılı! Hoşgeldin " + name);
+                System.out.println(">> Giriş Başarılı! Hoşgeldin " + name);
 
-                if (role.equals("student")) {
-                    int grade = Integer.parseInt(userData[4]);
-                    String type = userData[5];
-                    
-                    Student activeStudent;
-                    if (type.equalsIgnoreCase("grad")) {
-                        activeStudent = new GraduateStudent(user, name, grade, "Tez Konusu");
-                    } else {
-                        activeStudent = new Student(user, name, grade);
-                    }
-                   // eklenecek showStudentMenu(scanner, courseCatalog, activeStudent);
+                switch (role.toLowerCase()) {
+                    case "student":
+                        int grade = Integer.parseInt(userData[4]);
+                        String type = userData[5];
+                        
+                        Student activeStudent;
+                        if (type.equalsIgnoreCase("grad")) {
+                            activeStudent = new GraduateStudent(user, name, grade, "Tez Konusu");
+                        } else {
+                            activeStudent = new Student(user, name, grade);
+                        }
 
-                } else if (role.equals("instructor")) {
-                    String dept = userData[5];
-                    Instructor activeInstructor = new Instructor(name, dept);
-                    
-                    // Hoca girdiği an, katalogdaki dersleri kendine çeker.
-                    activeInstructor.syncCoursesFromCatalog(courseCatalog);
-                    
-                  // eklenecek  showInstructorMenu(scanner, courseCatalog, activeInstructor);
+                     // eklenecek   showStudentMenu(scanner, courseCatalog, activeStudent);
+                        break;
+
+                    case "instructor":
+                        String dept = userData[5];
+                        Instructor activeInstructor = new Instructor(name, dept);
+                        
+                        // Hoca girdiği an, katalogdaki dersleri kendine çeker (Senkronizasyon)
+                        activeInstructor.syncCoursesFromCatalog(courseCatalog);
+                        
+                      // eklenecek  showInstructorMenu(scanner, courseCatalog, activeInstructor);
+                        break;
+
+                    default:
+                        System.out.println("Hata: Tanımsız kullanıcı rolü!");
+                        break;
                 }
             } else {
-                System.out.println("Hatalı kullanıcı adı veya şifre!");
+                System.out.println("Hatalı kullanıcı adı veya şifre! Tekrar deneyin.");
             }
         }
     }
 }
-    
