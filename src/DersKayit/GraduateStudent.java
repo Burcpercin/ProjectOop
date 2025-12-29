cf2a4d98ff3cdb3eb1a5c69dc745da662d30e61c
burcp <burcp@Victus_16>
2025-12-29 16:49:41 +0300

    GraduateStudent şuan kullanılmasada tez konusu değişkenine getter eklendi. İleride Student classından ayrı anlamlı bir class olması için eklendi.

--- src/DersKayit/GraduateStudent.java
+++ src/DersKayit/GraduateStudent.java
@@ -3,13 +3,18 @@ package DersKayit;
 public class GraduateStudent extends Student {
     private String thesisTopic;
 
-    public GraduateStudent(String username, String name, int gradeLevel, String thesisTopic) {
-        super(username, name, gradeLevel);
+    // Main'den gelen username'i, Student sınıfındaki studentNumber yerine gönderiyoruz.
+    public GraduateStudent(String studentNumber, String name, int gradeLevel, String thesisTopic) {
+        super(studentNumber, name, gradeLevel);
         this.thesisTopic = thesisTopic;
     }
 
     @Override
     public double calculateTuition() {
-        return 8000.0; // Polimorfizm
+        return 8000.0; // Yüksek Lisans harcı
+    }
+    
+    public String getThesisTopic() {
+        return thesisTopic;
     }
 }
