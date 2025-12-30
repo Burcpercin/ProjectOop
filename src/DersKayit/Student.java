d50c437ac3f49b454847ca52afb5f1338ab09281
burcp <burcp@Victus_16>
2025-12-30 23:43:43 +0300

    CSV den kayıtlı dersleri çekmek ve GPA hesaplamak için gerekli kredi hesaplama fonksiyonları eklendi.

--- src/DersKayit/Student.java
+++ src/DersKayit/Student.java
@@ -8,8 +8,6 @@ public class Student implements Registrable {
     private String studentNumber; 
     private String name;
     private int gradeLevel;
-    
-    // Encapsulation: Liste private, dışarıdan doğrudan erişilemez.
     private List<Course> enrolledCourses;
 
     public Student(String studentNumber, String name, int gradeLevel) {
@@ -19,30 +17,41 @@ public class Student implements Registrable {
         this.enrolledCourses = new ArrayList<>();
     }
 
-    // Polimorfizm için metot
-    public double calculateTuition() {
-        return 5000.0;
+    public double calculateTuition() { return 5000.0; }
+
+    // GPA Hesaplama taslağı (ders notu girme henüz yok).
+    public int calculateTotalCredits() {
+        int total = 0;
+        for(Course c : enrolledCourses) {
+            total += c.getCredit();
+        }
+        return total;
     }
 
     @Override
     public void registerForCourse(Course course) {
-        // Asıl kontroller Registration sınıfında yapılır, burası sadece listeye ekler.
         if (!enrolledCourses.contains(course)) {
             enrolledCourses.add(course);
-            System.out.println("Sistem Mesajı: " + course.getCode() + " dersi öğrenci profiline işlendi.");
+            System.out.println(">> " + course.getCode() + " dersi profiline eklendi.");
+        }
+    }
+
+    //  Program açılışında kayıtlı dersleri csv dosyasından çekmek için
+    public void loadEnrolledCourse(Course course) {
+        if (!enrolledCourses.contains(course)) {
+            enrolledCourses.add(course);
         }
     }
 
     public void dropCourse(Course course) {
         if (enrolledCourses.contains(course)) {
             enrolledCourses.remove(course);
-            System.out.println("Sistem Mesajı: " + course.getCode() + " dersi bırakıldı.");
+            System.out.println(">> " + course.getCode() + " dersi bırakıldı.");
         } else {
-            System.out.println("Hata: Bu dersi zaten almıyorsunuz.");
+            System.out.println(">> Hata: Bu dersi zaten almıyorsunuz.");
         }
     }
 
-    // Read-only liste döndürür (Güvenlik)
     public List<Course> getEnrolledCourses() {
         return Collections.unmodifiableList(enrolledCourses);
     }
