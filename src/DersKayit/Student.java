47e4d46ac8d49efdc0ac12a091bbfb9e7d3a7c0d
burcp <burcp@Victus_16>
2025-12-31 06:55:19 +0300

    Notların girilmesi için map yapısı kuruldu. Not ekleme, görüntüleme metodları oluşturuldu. Oluşbilecek senaryolar için exeptionlar yazıldı.

--- src/DersKayit/Student.java
+++ src/DersKayit/Student.java
@@ -2,19 +2,61 @@ package DersKayit;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Student implements Registrable {
     private String studentNumber; 
     private String name;
     private int gradeLevel;
     private List<Course> enrolledCourses;
+    
+    // Notları tutan yapı (Ders Kodu -> Not Kaydı)
+    private Map<String, GradeRecord> courseGrades;
 
     public Student(String studentNumber, String name, int gradeLevel) {
         this.studentNumber = studentNumber;
         this.name = name;
         this.gradeLevel = gradeLevel;
         this.enrolledCourses = new ArrayList<>();
+        this.courseGrades = new HashMap<>(); // Map başlatılıyor
+    }
+
+    // Not Ekleme Metodu
+    public void addGrade(String courseCode, int midterm, int finalExam) {
+        // Not sadece alınan derslere girilebilir
+        GradeRecord record = new GradeRecord(midterm, finalExam);
+        courseGrades.put(courseCode, record);
+    }
+
+    // GPA Hesaplama
+    public double calculateGPA() {
+        double totalPoints = 0.0;
+        double totalCredits = 0.0;
+
+        for (Course course : enrolledCourses) {
+            // Eğer bu dersin notu girilmişse
+            if (courseGrades.containsKey(course.getCode())) {
+                GradeRecord grade = courseGrades.get(course.getCode());
+                double coefficient = grade.getCoefficient(); // 4.0, 3.5 vs.
+                int credit = course.getCredit();
+                
+                totalPoints += (coefficient * credit);
+                totalCredits += credit;
+            }
+        }
+
+        if (totalCredits == 0) return 0.0;
+        return totalPoints / totalCredits;
+    }
+    
+    // Öğrenci menüsünde notu göstermek için yardımcı metot
+    public String getGradeDetails(String courseCode) {
+        if(courseGrades.containsKey(courseCode)) {
+            return courseGrades.get(courseCode).toString();
+        }
+        return "Not Girilmedi";
     }
 
     public double calculateTuition() { return 5000.0; }
@@ -44,7 +86,8 @@ public class Student implements Registrable {
     public void dropCourse(Course course) {
         if (enrolledCourses.contains(course)) {
             enrolledCourses.remove(course);
-            course.decrementEnrollment(); // Dersi bırakınca kontenjan açılsın
+            course.decrementEnrollment(); // Kontenjanı aç
+            courseGrades.remove(course.getCode()); // Dersi bırakırsa notunu da sil
             System.out.println(">> " + course.getCode() + " dersi bırakıldı.");
         } else {
             System.out.println(">> Hata: Bu dersi zaten almıyorsunuz.");
