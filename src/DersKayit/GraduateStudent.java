c3466f16dda5547ec9479199bcb5e7b7ce3a4e4c
burcp <burcp@Victus_16>
2026-01-04 22:14:45 +0300

    Departman parametresi eklendi.

--- src/DersKayit/GraduateStudent.java
+++ src/DersKayit/GraduateStudent.java
@@ -3,9 +3,9 @@ package DersKayit;
 public class GraduateStudent extends Student {
     private String thesisTopic;
 
+    public GraduateStudent(String studentNumber, String name, int gradeLevel, String department, String thesisTopic) {
 
-    public GraduateStudent(String studentNumber, String name, int gradeLevel, String thesisTopic) {
-        super(studentNumber, name, gradeLevel, thesisTopic);
+        super(studentNumber, name, gradeLevel, department);
         this.thesisTopic = thesisTopic;
     }
 
